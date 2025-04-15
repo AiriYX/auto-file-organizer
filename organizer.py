c795bf8145337a0b36144d7e72ecc5ea9559ad99
Airi <kaylaweng103@gmail.com>
2025-04-15 15:57:26 -0400

    added logic for creating folder with keyword and moving

--- organizer.py
+++ organizer.py
@@ -2,11 +2,14 @@
 from file_utils import organize_files
 
 if __name__ == "__main__":
-    import argparse # what does this do?
+    import argparse # what does this do? (helps accept command-line arguments fromm the terminal)
 
     parser = argparse.ArgumentParser(description = "Sort files by keyword grouping.")
-    parser.add_argument("command", help=="Type 'sort' to organize your current directory.")
-    args = parser.parse.args()
+    parser.add_argument("command", help="Type 'sort' to organize your current directory.")
+    parser.add_argument("directory", help="Path to the directory you want to organize.")
+    args = parser.parse_args()
 
-    if args.command == "sort":
-        organize_files("args.directory")
+    if args.command == "tidyup":
+        organize_files(args.directory)
+    else:
+        print("Unknown command. Please use 'tidyup'.")
